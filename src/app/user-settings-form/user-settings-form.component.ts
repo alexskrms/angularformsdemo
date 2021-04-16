da435afb8ee3151663b2415fa90576dfc0304047
Alex Martir Sabater <alex@venomapps.com>
2021-04-16 12:20:28 +0200

    Added: -Two way Data Binding to our form.

--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserSettings } from '../data/user-settings';
 
 @Component({
   selector: 'app-user-settings-form',
@@ -7,6 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserSettingsFormComponent implements OnInit {
 
+  originalUserSettings : UserSettings = {
+    name: 'Alex',
+    emailOffers: true,
+    interfaceStyle: 'dark',
+    subscriptionType: 'Annual',
+    notes: 'Test notes'
+  };
+
+  userSettings : UserSettings = {
+    ...this.originalUserSettings
+  };
+  
   constructor() { }
 
   ngOnInit(): void {
